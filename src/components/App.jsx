8c40adaa88b176077e444b623450fe0d9498488e
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-09-25 12:24:26 +0300

    btn and stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = (option, event) => {
+    // console.log(event.target)
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.addFeedback}
+        />
+        <h2>Statistics</h2>
+        <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+          />
+      </div>
+    );
+  }
+}
